ce10fd37dcd27cca899a2658e698edb94787ffbc
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-06 20:15:13 +0100

    Removed Contact Page + Added Modal Instead

---
+++ index.js
@@ -0,0 +1,4 @@
+let travelDestinations = document.getElementsByClassName('travel-destination')
+let jumbotronSubheading = document.getElementById('jumbotron-subheading')
+
+jumbotronSubheading.innerText = `We Have ${travelDestinations.length} Great Destinations To Choose From`
