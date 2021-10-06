cc32fa7b41cd79dea4ef53ccce550b9cc6813333
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-06 20:38:09 +0100

    Added Function TO Remove All Cards

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ let travelDestinations = document.getElementsByClassName('travel-destination')
 let jumbotronSubheading = document.getElementById('jumbotron-subheading')
 
 jumbotronSubheading.innerText = `We Have ${travelDestinations.length} Great Destinations To Choose From`
+
+
+const removeCards = () => {
+    let cards = document.querySelectorAll('.card')
+    
+    for (let card of cards) {
+        card.remove()
+    }
+}
